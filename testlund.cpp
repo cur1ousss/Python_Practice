27e5bde3741bf1af2ba59ca3abb44e0367f2301e
cybrpnkkrtos <abhi110401@gmail.com>
2021-09-30 09:40:56 +0530

    Manage Multiple Env activate env env.yaml conept environment variables in activate.d deactivate.d env_vars.sh

---
+++ testlund.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+	return 0;
+}
+
+/*
+int main(){
+	int neg[]={10,20,30};
+	cout<<-2[neg];
+	cout<<-neg[2];
+	return 0;
+}*/
---
+++ testsc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+
+	
+}
+/*
+int main(){
+	int digit=97;
+	switch(digit){
+		case 'a':
+		printf("yes");
+		break;
+		case 2:
+		printf("no\n");
+		break;
+	}
+}
+*/
